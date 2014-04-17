3f08ce32d6989f741b93899b1e0eaa14891e5579
jcpesd <jcpesd@github>
2014-04-17 08:05:14 +0000

    bring server to a compiling, listening state

--- Server/server.cpp
+++ Server/server.cpp
@@ -1,19 +1,73 @@
+#include <QtGui>
+#include <QtNetwork/QtNetwork>
+
 /*TODO: import qt, setup qt, write QT main function*/
 
-int main()
+typedef struct
 {
+}
+Queue;
+
+typedef struct
+{
+}
+FolderList;
+
+typedef struct
+{
+}
+Socket;
+
+class Server: public QObject
+{
+	private:
+		Q_OBJECT
+	public:
+		Server(QObject* parent=nullptr)
+		{
+			setParent(parent);
+			quit=false;
+			client_sockets=std::vector<QTcpSocket*>();
+			listen_socket=new QTcpServer(this);
+			listen_socket->connect(listen_socket, SIGNAL(newConnection()), this, SLOT(client_connected_cb()));
+			if(!listen_socket->listen(QHostAddress::Any, 8888))
+			{
+				QMessageBox::critical(nullptr, "Social DJ", listen_socket->errorString());
+			}
+		}
+		bool quit;
+		std::vector<QTcpSocket*> client_sockets;
+		QTcpServer* listen_socket;
+	public slots:
+		void quit_cb()
+		{
+			quit=true;
+		}
+		void client_connected_cb()
+		{
+			printf("called\n");
+			QTcpSocket *client=listen_socket->nextPendingConnection();
+			client_sockets.push_back(client);
+			client->write("new_song|1\n");
+		}
+};
+
+int main(int argc, char** argv)
+{
+	QApplication app(argc, argv);
 	Queue queue;
-	FolderList folders(queue);
-	folders.add_folder_by_choosing();
+	FolderList folders;
 	
-	Socket listen_socket=listen(port#);
+	auto s=new Server(&app);
+	//folders.add_folder_by_choosing();
 	
-	Socket client_sockets[];
-	bool quit=false;
-	while(!quit)
+	app.exec();
+	return 0;
+	while(!s->quit)
 	{
-		select(listen_socket, sockets, qt)
-		switch(source)
+		//select(listen_socket, sockets, qt)
+		//listen_socket->exec();
+		/*switch(source)
 		{
 			case qt:
 				if(message==do_quit)
@@ -43,7 +97,9 @@ int main()
 						break;
 				}
 				break;
-		}
+		}*/
 	}
 	return 0;
 }
+
+#include "server.moc"
