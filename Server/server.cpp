fcebdf3869dc2a1fa3e101df689a9ce54c593702
jcpesd <jcpesd@github>
2014-04-17 08:38:10 +0000

    fix newlines in messages and sketch out serverside state tracking

--- Server/server.cpp
+++ Server/server.cpp
@@ -1,7 +1,6 @@
 #include <QtGui>
 #include <QtNetwork/QtNetwork>
-
-/*TODO: import qt, setup qt, write QT main function*/
+#include <unordered_set>
 
 typedef struct
 {
@@ -10,35 +9,53 @@ Queue;
 
 typedef struct
 {
+	void FolderList(){};
+	void add_folders_by_choosing();
+	void add_folder_by_path(const char* path);
 }
 FolderList;
 
+typedef unsigned int id;
+
 typedef struct
 {
+	std::unordered_set<id> songs;
+	std::unordered_set<id> albums;
+	std::unordered_set<id> artists;
+	QTcpSocket* socket;
 }
-Socket;
+Client;
+
+typedef struct _song Song;
 
 class Server: public QObject
 {
 	private:
 		Q_OBJECT
 	public:
+		bool quit;
+		std::vector<Client*> clients;
+		QTcpServer* listen_socket;
+		Queue queue;
+		FolderList folders;
 		Server(QObject* parent=nullptr)
 		{
 			setParent(parent);
 			quit=false;
-			client_sockets=std::vector<QTcpSocket*>();
+			clients=std::vector<Client*>();
 			listen_socket=new QTcpServer(this);
 			listen_socket->connect(listen_socket, SIGNAL(newConnection()), this, SLOT(client_connected_cb()));
 			if(!listen_socket->listen(QHostAddress::Any, 8888))
 			{
 				QMessageBox::critical(nullptr, "Social DJ", listen_socket->errorString());
+				quit=true;
 			}
 		}
-		bool quit;
-		std::vector<QTcpSocket*> client_sockets;
-		QTcpServer* listen_socket;
 	public slots:
+		void new_song(Song* s)
+		{
+			//add the song to the database; send the song to all quiescent clients
+		}
 		void quit_cb()
 		{
 			quit=true;
@@ -46,30 +63,35 @@ class Server: public QObject
 		void client_connected_cb()
 		{
 			printf("called\n");
-			QTcpSocket *client=listen_socket->nextPendingConnection();
-			client_sockets.push_back(client);
-			client->write("new_song|1\n");
-			client->write("new_artist|1\n");
-			client->write("artist_info|1|The Physics\n");
-			client->write("new_album|1\n");
-			client->write("album_info|1|High Society EP|1\n");
-			client->write("song_info|1|album|1|artist|1|duration|173.2\n");
-			client->write("song_info|1|title|The Session\n");
-			client->write("add_bottom|1");
-			client->write("score|1|4");
-			client->write("new_song|2\n");
-			client->write("new_artist|2\n");
-			client->write("artist_info|2|Miami Horror\n");
-			client->write("song_info|2|artist|2|duration|251.1|title|Sometimes\n");
-			client->write("add_bottom|2");
+			auto c=new Client();
+			c->socket=listen_socket->nextPendingConnection();
+			clients.push_back(c);
+			
+			auto socket=c->socket;
+			socket->write("new_song|1\n");
+			socket->write("new_artist|1\n");
+			socket->write("artist_info|1|The Physics\n");
+			socket->write("new_album|1\n");
+			socket->write("album_info|1|High Society EP|1\n");
+			socket->write("song_info|1|album|1|artist|1|duration|173.2\n");
+			socket->write("song_info|1|title|The Session\n");
+			socket->write("add_bottom|1\n");
+			socket->write("score|1|4\n");
+			socket->write("new_song|2\n");
+			socket->write("new_artist|2\n");
+			socket->write("artist_info|2|Miami Horror\n");
+			socket->write("song_info|2|artist|2|duration|251.1|title|Sometimes\n");
+			socket->write("add_bottom|2\n");
+		}
+		void client_message(Client* c)
+		{
+			
 		}
 };
 
 int main(int argc, char** argv)
 {
 	QApplication app(argc, argv);
-	Queue queue;
-	FolderList folders;
 	
 	auto s=new Server(&app);
 	//folders.add_folder_by_choosing();
@@ -83,8 +105,6 @@ int main(int argc, char** argv)
 		/*switch(source)
 		{
 			case qt:
-				if(message==do_quit)
-					quit=1
 				if(message==do_choose)
 					folders.add_folder_by_choosing();
 				break;
