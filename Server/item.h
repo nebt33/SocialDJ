026ca584176d423e79eb73ebd004111af4f8a6a6
jcpesd <jcpesd@github.com>
2014-04-26 18:46:52 +0000

    flesh out item structs

--- Server/item.h
+++ Server/item.h
@@ -1,31 +1,50 @@
 #ifndef SONG_H
 #define SONG_H
+#include <vector>
+
 //the zero ID refers to a nonexistent object. Artist, Album, and Song IDs are in separate namespaces such that Song 1 and Album 1 have no a priori dependence.
 typedef unsigned int id;
 
-typedef struct
+struct Album
 {
-	const char* get_name() const;//may be NULL, e.g. if the album title is unknown
-	unsigned int get_n_tracks() const;//returns how many tracks the album has; if we don't have all tracks on the album this would be the highest known track number
-	const id* get_tracks() const;//returns an array of track IDs, with 0s in the spots where no track is known
-	id get_id() const;
-}
-Album;
+	Album(id b, const char* name);
+	void add_track(id t){};
 
-typedef struct
-{
-	const char* get_name() const;
-	id get_id() const;
-}
-Artist;
+	const char* get_name() const {return this->name;};//may be NULL, e.g. if the album title is unknown
+	unsigned int get_n_tracks() const {return this->tracks.size();};//returns how many tracks the album has; if we don't have all tracks on the album this would be the highest known track number
+	const id* get_tracks() const {return this->tracks.data();};//returns an array of track IDs, with 0s in the spots where no track is known
+	id get_id() const {return this->bid;};
+	
+	id bid;
+	char* name=nullptr;
+	std::vector<id> tracks;
+};
 
-typedef struct
+struct Artist
 {
-	id get_album() const;//may be 0
-	id get_artist() const;//may be 0
-	const char* get_title() const;
-	unsigned int get_duration() const;//may be 0 if unknown, integer is in tenths of a second
-	id get_id() const;
-}
-Song;
+	Artist(id a, const char* name);
+	const char* get_name() const {return this->name;};
+	id get_id() const {return this->aid;};
+	
+	id aid;
+	char* name=nullptr;
+};
+
+struct Song
+{
+	Song(id s, id album, id artist, char* title);
+	void set_duration(unsigned int tenths){this->duration = tenths;}
+	
+	id get_album() const {return this->album;};//may be 0
+	id get_artist() const {return this->artist;};//may be 0
+	const char* get_title() const {return this->title;};
+	unsigned int get_duration() const {return this->duration;};//may be 0 if unknown, integer is in tenths of a second
+	id get_id() const {return this->sid;};
+	
+	id sid;
+	id album;
+	id artist;
+	char* title=nullptr;
+	unsigned int duration=0;
+};
 #endif
