29f2f45d588be0076e06efb9a4e537c77af3732a
jma454 <jma454@mst.edu>
2014-05-02 01:02:00 -0500

    updating folders when changed

--- Server/FolderList.cpp
+++ Server/FolderList.cpp
@@ -13,12 +13,11 @@
 #include <QIODevice>
 #include <QMessageBox>
 #include <queue>
-#define ID3LIB_LINKOPTION 1
-#include <id3/tag.h>
 
 FolderList::FolderList(Database& thedb)
 {
 	db = &thedb;
+	connect(&fileWatcher, SIGNAL(directoryChanged(QString)), this, SLOT(rescanDir(QString)));
 }
 
 //TODO: remove parent parameter if this is the way we do it
@@ -38,32 +37,37 @@ void FolderList::add_folders_by_choosing(QWidget* parent)
 	if( dialog.exec() )
 	{
 		QStringList dirs = dialog.selectedFiles();
-		QStringList::const_iterator it;
-		for (it = dirs.begin(); it != dirs.end(); it++)
-		{
-			QDir *dir = new QDir(QString(*it));
-			std::queue<QString> queue;
-			queue.push(dir->absolutePath());
-			QStringList results;
-			while (!queue.empty()) {
-				auto const subdir_name = queue.front();
-				queue.pop();
-				results.push_back(subdir_name);
-				QDir subdir(subdir_name);
-		 
-				auto const &directories = subdir.entryInfoList({}, QDir::AllDirs | QDir::NoSymLinks | QDir::NoDotAndDotDot);
-				for (auto const &data : directories) {
-					queue.push(data.absoluteFilePath());
-				}
-			}
-			for(int i = 0; i < results.size(); i++)
-			{
-				fileWatcher.addPath(results[i]);
-			}
-			scanDirs(results);
-		}
-		QMessageBox::information(0, QString("Social DJ"), QString("Done Adding Folders"));
+		add_folders(dirs);
 	}
+}
+
+void FolderList::add_folders(QStringList paths)
+{
+	QStringList::const_iterator it;
+	for (it = paths.begin(); it != paths.end(); it++)
+	{
+		QDir *dir = new QDir(QString(*it));
+		std::queue<QString> queue;
+		queue.push(dir->absolutePath());
+		QStringList results;
+		while (!queue.empty()) {
+			auto const subdir_name = queue.front();
+			queue.pop();
+			results.push_back(subdir_name);
+			QDir subdir(subdir_name);
+	 
+			auto const &directories = subdir.entryInfoList({}, QDir::AllDirs | QDir::NoSymLinks | QDir::NoDotAndDotDot);
+			for (auto const &data : directories) {
+				queue.push(data.absoluteFilePath());
+			}
+		}
+		for(int i = 0; i < results.size(); i++)
+		{
+			fileWatcher.addPath(results[i]);
+		}
+		scanDirs(results);
+	}
+	QMessageBox::information(0, QString("Social DJ"), QString("Done Adding Folders"));
 }
 
 void FolderList::initFolderList()
@@ -111,72 +115,13 @@ void FolderList::scanDirs(QStringList dirs)
 		QStringList files = dir->entryList(QDir::Files);
 		for(int i = 0; i < files.size(); i++) 
 		{
-			if(files[i].endsWith(".mp3"))
-			{
-				const char* song = NULL;
-				const char* album = NULL;
-				const char* artist = NULL;
-				unsigned int index = 0;
-				unsigned int duration = 0;
-				//get id3 info and add song to database
-				ID3_Tag tag((dir->absolutePath() + "/" + files[i]).toUtf8().constData());
-				if(tag.NumFrames() > 0)
-				{
-					//get song title
-					ID3_Frame* frame = tag.Find(ID3FID_TITLE);
-					if( NULL != frame)
-					{
-						ID3_Field* field = frame->GetField(ID3FN_TEXT);
-						song = field->GetRawText();
-					}
-					qDebug() << "title: "<<song;
-					//get album name
-					frame = tag.Find(ID3FID_ALBUM);
-					if( NULL != frame)
-					{
-						ID3_Field* field = frame->GetField(ID3FN_TEXT);
-						album = field->GetRawText();
-					}
-					qDebug() << "album: "<<album;
-					//get artist name
-					frame = tag.Find(ID3FID_LEADARTIST);
-					if( NULL != frame)
-					{
-						ID3_Field* field = frame->GetField(ID3FN_TEXT);
-						artist = field->GetRawText();
-					}
-					qDebug() << "artist: "<<artist;
-					//get song index on album
-					frame = tag.Find(ID3FID_TRACKNUM);
-					if( NULL != frame)
-					{
-						ID3_Field* field = frame->GetField(ID3FN_TEXT);
-						const char* temp = field->GetRawText();
-						sscanf(temp, "%d/",&index);
-					}
-					qDebug() << "index: "<<index;
-					qDebug() << "\n";
-				}
-				if(song == NULL)
-				{
-					song = files[i].toUtf8().constData();
-				}
-				
-				id artistId;
-				if(artist != NULL )
-					artistId = db->add_artist(artist);
-				else
-					artistId = 0;
-					
-				id albumId;
-				if(album != NULL )
-					albumId = db->add_artist(album);
-				else
-					albumId = 0;
-					
-				id newId = db->add_song();
-				db->update_song(newId, song, artistId, albumId, index, duration);
-			}
+			Database::addSongFromPath((dir->absolutePath() + "/" + files[i]).toUtf8().constData(), *db);
 		}
 	}
+}
+
+void FolderList::rescanDir(const QString &path)
+{
+	qDebug()<<"rescanning dir";
+	add_folders(QStringList(path));
 }
