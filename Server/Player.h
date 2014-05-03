5eb04d87f54c15eeb8ed8489b0adcdde281d657f
wviers <treyviers@gmail.com>
2014-05-03 14:42:36 -0500

    fixed a few possible bugs and commented

--- Server/Player.h
+++ Server/Player.h
@@ -20,16 +20,17 @@ struct Player : public QObject
 			player = new QMediaPlayer(0);
 			playlist = new QMediaPlaylist(0);
 			
+			//Playback starts from the current and moves through each successive item 
+			//until the last is reached and then stops. The next item is a null item 
+			//when the last one is currently playing.
 			playlist->setPlaybackMode(QMediaPlaylist::Sequential);
 			player->setPlaylist(playlist);
-
-			player->play();
 		}	  
 		
 		//Called by the queue when a new song is moved to the top of the queue
 		void newSong(const Song *song);
 		void next();
-		void Play();
+		void play();
 		void pause();
 };
 
